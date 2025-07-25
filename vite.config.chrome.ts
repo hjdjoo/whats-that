ffb644bcec5826237a89b5cab8b5c2ac898d1192
Darryl <hjdjoo@gmail.com>
2025-07-25 14:56:39 -0400

    added config for chromium env

--- vite.config.chrome.ts
+++ vite.config.chrome.ts
@@ -13,7 +13,7 @@ export default mergeConfig(
         manifest: {
           ...baseManifest,
           background: {
-            service_worker: 'src/pages/background/index.ts',
+            service_worker: 'src/background/index.ts',
             type: 'module'
           },
         } as ManifestV3Export,
